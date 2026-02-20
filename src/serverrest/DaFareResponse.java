2983239087be9771176757ddaa38da7d360fa9ba
Grilli Nico <150380049+SorciusPrime@users.noreply.github.com>
2026-02-20 09:23:19 +0100

    creazione dati response

--- src/serverrest/DaFareResponse.java
+++ src/serverrest/DaFareResponse.java
@@ -10,17 +10,43 @@ package serverrest;
  */
 public class DaFareResponse {
 
-    
+    String giocata;
+    Integer numero;
+    String vittoria;
     // Costruttore vuoto necessario per GSON
     public DaFareResponse() {
     }
     
     // Costruttore con parametri
-   
+    public DaFareResponse(String giocata, Integer numero, String vittoria) {
+        this.giocata = giocata;
+        this.numero = numero;
+        this.vittoria = vittoria;
+    }  
     
     // Getter
-   
+    public String getGiocata() {
+        return giocata;
+    }
+
+    public void setGiocata(String giocata) {
+        this.giocata = giocata;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
     
     // Setter
-    
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getVittoria() {
+        return vittoria;
+    }
+
+    public void setVittoria(String vittoria) {
+        this.vittoria = vittoria;
+    }
 }
