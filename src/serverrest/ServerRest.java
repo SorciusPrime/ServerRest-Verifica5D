2996441ae47c081d1ad81e397c053f414055cee8
Grilli Nico <150380049+SorciusPrime@users.noreply.github.com>
2026-04-08 18:33:40 +0200

    cambio porta
    la porta è stata cambiata da 0 a 8080

--- src/serverrest/ServerRest.java
+++ src/serverrest/ServerRest.java
@@ -50,8 +50,8 @@ public class ServerRest {
             System.out.println("Porta: " + porta);
             System.out.println();
             System.out.println("Endpoint disponibili:");
-            System.out.println("  - POST: http://localhost:" + porta + "/DA FARE");
-            System.out.println("  - GET:  http://localhost:" + porta + "/DA FARE");
+            System.out.println("  - POST: http://localhost:" + porta + "/api/v1/roulette/post");
+            System.out.println("  - GET:  http://localhost:" + porta + "/api/v1/roulette/get");
             System.out.println("  - Info: http://localhost:" + porta + "/");
             System.out.println();
             System.out.println();
